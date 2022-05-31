d5585e7d305a3425b35b166cd4d982230bc03581
prasanthg23 <psanthg@amazon.com>
2022-05-31 10:32:07 -0700

    second commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,12 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "psanthg+aftnewaccnt@amazon.com"
-    AccountName               = "Aftnew-accnt"
-    ManagedOrganizationalUnit = "AFT"
-    SSOUserEmail              = "psanthg+aftnewaccnt@amazon.com"
-    SSOUserFirstName          = "aftnew"
-    SSOUserLastName           = "account"
+    AccountEmail              = "psanthg+orgnestedaccnt@amazon.com"
+    AccountName               = "Aftorg-accnt"
+    ManagedOrganizationalUnit = "Nested OU"
+    SSOUserEmail              = "psanthg+orgnestedaccnt@amazon.com"
+    SSOUserFirstName          = "aftorg"
+    SSOUserLastName           = "account1"
   }
 
   account_tags = {
@@ -23,5 +23,5 @@ module "sandbox" {
     group = "non-prod"
   }
 
-  account_customizations_name = "Aftnew-accnt"
+  account_customizations_name = "Aftorg-accnt"
 }
